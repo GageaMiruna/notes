2d352ec5c071ea7b4c0c8ee39fb8f07002449e25
GageaMiruna <gageamiruna@gmail.com>
2019-06-05 13:16:03 +0300

    fixed tags

--- src/MyNotes.js
+++ src/MyNotes.js
@@ -2,23 +2,23 @@ import {LitElement, html, css } from 'lit-element';
 import './SingleNote'
 export class MyNotes extends LitElement{
 
-    static get styles(){
-        return css`
-        :host {
-            background:peachpuff;
-            display:flex;
-            flex-wrap: wrap;
-            align-content:flex-start;
-            width:inherit;
-            height:inherit;
-        }
-        `;
-    }
+	static get styles(){
+		return css`
+		:host {
+			background:peachpuff;
+			display:flex;
+			flex-wrap: wrap;
+			align-content:flex-start;
+			width:inherit;
+			height:inherit;
+		}
+		`;
+	}
 
-    render(){
-        return html`
-        <slot></slot>
-        `;
-    }
+	render(){
+		return html`
+		<slot></slot>
+		`;
+	}
 }
 
